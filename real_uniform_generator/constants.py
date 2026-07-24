f813dee4ad08e1b8cd2c304b3255827046fdc549
tomicodev <tomico@tomico.me>
2026-07-25 03:12:21 +0900

    Add addon constants and collection helpers

---
+++ real_uniform_generator/constants.py
@@ -0,0 +1,13 @@
+ADDON_ID = "real_uniform_generator"
+ADDON_NAME = "Real Uniform Generator"
+VERSION = (0, 2, 0)
+
+GENERATED_COLLECTION = "RUG_UniformSkirt"
+PREVIEW_COLLECTION = "RUG_Preview"
+EXPORT_COLLECTION = "RUG_ExportTemp"
+ROOT_OBJECT = "RUG_UniformSkirt_Root"
+
+FABRIC_MATERIAL = "RUG_UniformFabric"
+LINING_MATERIAL = "RUG_Lining"
+THREAD_MATERIAL = "RUG_Thread"
+METAL_MATERIAL = "RUG_Metal"
